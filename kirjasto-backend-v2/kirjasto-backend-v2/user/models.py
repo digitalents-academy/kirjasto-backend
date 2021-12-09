44a26fe6c0ea49a671677acdae0f2b97ea32ae76
BorisHiltunen <90689146+BorisHiltunen@users.noreply.github.com>
2021-12-09 22:19:54 +0200

    Add changes

--- kirjasto-backend-v2/kirjasto-backend-v2/user/models.py
+++ kirjasto-backend-v2/kirjasto-backend-v2/user/models.py
@@ -1,9 +1,15 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, session, redirect
 from passlib.hash import pbkdf2_sha256
 from app import db
 import uuid
 
 class User:
+
+    def start_session(self, user):
+        del user['password']
+        session['logged_in'] = True
+        session['user'] = user
+        return jsonify(user), 200
 
     def signup(self):
         
@@ -23,7 +29,22 @@ class User:
             return jsonify({ "error": "Email address already in use" }), 400
 
         if db.users.insert_one(user):
-            return jsonify(user), 200
+            return self.start_session(user)
 
         #return jsonify(user), 200
         return jsonify({ "error": "Signup failed"}), 400
+
+    def signout(self):
+        session.clear()
+        return redirect('/')
+
+    def login(self):
+
+        user = db.users.find_one({
+            "email": request.form.get('email')
+        })
+
+        if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
+            return self.start_session(user)
+
+        return jsonify({ "error": "Invalid login credentials" }), 401
